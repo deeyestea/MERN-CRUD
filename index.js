8714f85aa74a53db2ac4605f41568d53184e1c8f
desaigouda.tammanagoudra@teamlease.com-at-143912951401 <desaigouda.tammanagoudra@teamlease.com>
2024-04-29 21:48:14 +0530

    Made changes for mongodb URL

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const { connectMongoDb } = require('./connection');
 const app = express();
 const PORT = 8000;
 
-connectMongoDb('mongodb+srv://desai:desai1234@cluster1.mkc6h5a.mongodb.net/mern-practice').then(() => console.log("MongoDB Connected...")).catch((err) => console.log("Mongo Error: ", err))
+connectMongoDb('mongodb+srv://desai:<password>@cluster1.mkc6h5a.mongodb.net/mern-practice').then(() => console.log("MongoDB Connected...")).catch((err) => console.log("Mongo Error: ", err))
 
 app.use(express.urlencoded({ extended: false }))
 
